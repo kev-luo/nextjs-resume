91116201f96d408705c4584724e150e326e8191e
Kevin Luo <kvn.luo@gmail.com>
2020-12-16 22:30:30 -0500

    create custom scalars url and date

--- src/allTypes/index.ts
+++ src/allTypes/index.ts
@@ -1,3 +1,16 @@
+import { decorateType } from "@nexus/schema";
+import { GraphQLDate, GraphQLURL } from "graphql-scalars";
+
+export const GQLDate = decorateType(GraphQLDate, {
+  rootTyping: "Date",
+  asNexusMethod: "date",
+})
+
+export const GQLURL = decorateType(GraphQLURL, {
+  rootTyping: "URL",
+  asNexusMethod: "url",
+})
+
 // this imports then imediately exports all exports from ./Query
 export * from "./Query"
 export * from "./Bio";
