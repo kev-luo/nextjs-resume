072c49c3748b1941f23a0df46b43f03dd69b7f67
Kevin Luo <kvn.luo@gmail.com>
2020-12-16 22:38:49 -0500

    defining interfaces

--- src/allTypes/Bio.ts
+++ src/allTypes/Bio.ts
@@ -5,5 +5,10 @@ export const Bio = objectType({
   definition(t) {
     t.string("name");
     t.string("tagline");
+    t.string("email");
+    t.string("objective");
+    t.url("github", {resolve: (parent) => new URL(parent.github)});
+    t.url("website", {resolve: (parent) => new URL(parent.website)});
+    t.url("linkedin", {resolve: (parent) => new URL(parent.linkedin)});
   }
 })
